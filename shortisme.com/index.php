06a8c876fa1808a20b199a3dad862f6ea9fbe9e0
varaveza <69433965+varaveza@users.noreply.github.com>
2025-08-15 10:03:48 +0700

    last

--- shortisme.com/index.php
+++ shortisme.com/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Putih Kosong</title>
+    <title> I </title>
     <style>
 
         body {
@@ -30,7 +30,7 @@
 <body>
     <?php
 
-        echo str_repeat("hayolo ", 50000);
+        echo str_repeat("hayolo<br>", 50000);
     ?>
 </body>
 </html>
