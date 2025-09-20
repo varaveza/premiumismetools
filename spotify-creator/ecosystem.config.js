76cbf87788ec03d6825f2805f74fd8f0969b5c5e
varaveza <varaveza@github.com>
2025-09-20 12:08:48 +0700

    p

--- spotify-creator/ecosystem.config.js
+++ spotify-creator/ecosystem.config.js
@@ -11,12 +11,14 @@ module.exports = {
     env: {
       NODE_ENV: 'production',
       FLASK_ENV: 'production',
-      PORT: 5112
+      PORT: 5112,
+      CAPSOLVER_API_KEY: 'CAP-6E976677CE849E6F8CE434D5E64DA95D4F527360EEBDB0E849DA3F008A3E59BE'
     },
     env_production: {
       NODE_ENV: 'production',
       FLASK_ENV: 'production',
-      PORT: 5112
+      PORT: 5112,
+      CAPSOLVER_API_KEY: 'CAP-6E976677CE849E6F8CE434D5E64DA95D4F527360EEBDB0E849DA3F008A3E59BE'
     },
     log_file: '/var/www/premiumisme.co/api/logs/combined.log',
     out_file: '/var/www/premiumisme.co/api/logs/out.log',
